c2f45580cf24339a66b2926175a281d5f2b8d6e8
jayin92 <jayin920805@gmail.com>
2023-10-03 17:00:51 +0800

    Done bev.py
    Use warping skill to finish. There may be some error with coordinate (pitch/roll).

--- hw1/bev.py
+++ hw1/bev.py
@@ -21,8 +21,44 @@ class Projection(object):
             Project the top view pixels to the front view pixels.
             :return: New pixels on perspective(front) view image
         """
-
         ### TODO ###
+        W, H, depth, focal = 512, 512, 2.5, 256 # focal is computed from fov, and WH
+        roll = -(np.pi / 2)
+        cos_val = np.cos(roll)
+        if np.isclose(cos_val, 0, atol=1e-10):
+            cos_val = 0.0
+        sin_val = np.sin(roll)
+        if np.isclose(sin_val, 0, atol=1e-10):
+            sin_val = 0.0
+        
+        top_homo_c2w = np.eye(4)
+        top_homo_c2w[:3, :3] = np.array([[1, 0, 0], [0, cos_val, -sin_val], [0, sin_val, cos_val]]) # rotation
+        top_homo_c2w[:3, 3] = top_T = np.array([0, -2.5, 0]) # translation
+        
+        front_homo_c2w = np.eye(4)
+        front_homo_c2w[:3, 3] = np.array([0, -1, 0])
+        front_homo_w2c = np.linalg.inv(front_homo_c2w)
+        
+        c2c = np.matmul(front_homo_w2c, top_homo_c2w)
+        
+
+        
+        uv = np.array(points)
+        # top_image_coord to top_camera_coord
+        x = (uv[:, 0:1] - W*.5) * depth / focal
+        y = (uv[:, 1:] - H*.5) * depth / focal
+        z = np.full(x.shape, depth)
+        xyz = np.concatenate([x, y, z, np.ones(x.shape)], -1)
+        xyz = xyz[:, :, np.newaxis] # homogeneous
+        # top_camera_coord to front_camera_coord
+        c2c = np.tile(c2c, (uv.shape[0], 1, 1))
+        xyz = np.matmul(c2c, xyz)
+        # front_camera_coord to front_image_coord
+        u = xyz[:, 0] / xyz[:, 2] * focal + W*.5
+        v = xyz[:, 1] / xyz[:, 2] * focal + H*.5
+        new_pixels = np.round(np.concatenate([u, v], -1)).astype(int).tolist()
+
+        print("new_pixels", new_pixels)
         return new_pixels
 
     def show_image(self, new_pixels, img_name='projection.png', color=(0, 0, 255), alpha=0.4):
@@ -80,7 +116,6 @@ if __name__ == "__main__":
     cv2.setMouseCallback('image', click_event)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
-
     projection = Projection(front_rgb, points)
     new_pixels = projection.top_to_front(theta=pitch_ang)
     projection.show_image(new_pixels)
